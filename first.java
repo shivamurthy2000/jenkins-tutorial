e4c52f428cd522f78d2e5129512cf357992410b6
shivamurthy2000 <157881346+shivamurthy2000@users.noreply.github.com>
2026-04-05 14:43:49 +0530

    Create first.java
    sumne practise ge

---
+++ first.java
@@ -0,0 +1,15 @@
+class HelloWorld {
+
+    // Method to print message
+    void printMessage() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        // Create object of class
+        HelloWorld obj = new HelloWorld();
+        
+        // Call method
+        obj.printMessage();
+    }
+}
